c21958355b8d74680ed868a4cf417d2ceb4b47e6
moeenpatel <123477545+moeenpatel@users.noreply.github.com>
2023-05-23 18:37:03 +0530

    Add files via upload

---
+++ Brick-Breaker_Game/src/main/java/com/mycompany/brick/breaker_game/main.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.brick.breaker_game;
+//package com.mycompany.brick;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author chinm
+ */
+public class main {
+    public static void main(String[] args) {
+        JFrame obj = new JFrame();
+        GamePlay gameplay = new GamePlay();
+        obj.setBounds(10,10,700,600);
+        obj.setTitle("BrickBreaker");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gameplay);
+    }
+    
+}
